2b6334cb434a90210fdae7ce889fb18d76d12247
sammyvik <sammyvikug@gmail.com>
2024-07-29 00:46:23 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const express = require('express');
-const cors = require('cors');
 const admin = require('firebase-admin');
+
 const app = express();
 const PORT = 3000;
 
