5649405fff4ef6927146eafc3822d71fa8d7419b
sammyvikuganda <sammyvikug@gmail.com>
2025-04-28 19:39:47 +0300

    Update

--- index.js
+++ index.js
@@ -13,14 +13,17 @@ require('dotenv').config(); // load .env file
 const app = express();
 
 // Initialize Firebase Admin SDK
-admin.initializeApp({
-    credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-});
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        }),
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+    });
+}
+
 
 const db = admin.database();
 const publicKey = process.env.TEZA_PUBLIC_KEY;
